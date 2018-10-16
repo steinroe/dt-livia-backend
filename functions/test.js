84fbb44e3b2a34cf4fa8e02313e375b0b2ae960f
Steinroe <philipp@steinroetter.com>
2018-10-16 16:16:52 +0200

    More test data

--- functions/test.js
+++ functions/test.js
@@ -5,3 +5,15 @@ enhanceNotesWithLinks({
     content: 'The doctor just came in and told me I defeated the cancer!',
     timestamp: moment().toString()
 })
+
+enhanceNotesWithLinks({
+    title: 'My Second Note',
+    content: 'I have a condom on my head!',
+    timestamp: moment().toString()
+})
+
+enhanceNotesWithLinks({
+    title: 'I am sleeping bad',
+    content: 'My sleep got worse!',
+    timestamp: moment().toString()
+})
